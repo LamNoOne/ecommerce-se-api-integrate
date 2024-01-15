6dffce8b5de3ceb8aeedd7741896ce00a29684b8
LamNoOne <trannguyendaclam@gmail.com>
2024-01-15 14:18:49 +0700

    persist-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./app/store";
+import { store, persistor } from "./app/store";
 import Router from "./routes";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <Router />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     );
 };
--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,50 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice";
 import wishlistReducer from "../features/wishlist/wishlistSlice";
 
+import storage from "redux-persist/lib/storage";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    wishlist: wishlistReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
-        wishlist: wishlistReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }).concat(apiSlice.middleware),
     devTools: true,
 });
+
+export const persistor = persistStore(store);
