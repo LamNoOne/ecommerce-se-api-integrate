45b6998c6b7b46a2ef0ca719424e2f0ca00c2bcd
LamNoOne <trannguyendaclam@gmail.com>
2024-02-19 22:05:09 +0700

    'demo-info'

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -6,13 +6,14 @@ const Login = () => {
         <section>
             <div className="flex w-full h-auto justify-between items-center py-16">
                 <div className="overflow-hidden">
-                    <img
-                        className="w-[805px] object-contain"
-                        src={images.shop_auth}
-                        alt="shop-auth"
-                    />
+                    <img className="w-[805px] object-contain" src={images.shop_auth} alt="shop-auth" />
                 </div>
                 <div className="me-[135px] min-w-[371px]">
+                    <div class="demo text-start">
+                        <h4 className="text-black text-opacity-50">Demo:</h4>
+                        <p className="text-black text-opacity-50">- username: clientclient</p>
+                        <p className="text-black text-opacity-50">- password: 123456</p>
+                    </div>
                     <LoginForm />
                 </div>
             </div>
